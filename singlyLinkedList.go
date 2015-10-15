706575ebd7dd4729212dada7f1e8e0db027134e9
ken5scal <kengoscal@gmail.com>
2015-10-15 20:24:30 +0900

    Implemented SinglyLinkedList

--- singlyLinkedList.go
+++ singlyLinkedList.go
@@ -1,5 +1,7 @@
 package linkedList
 
+import "fmt"
+
 type SinglyLinkedList struct {
 	head, tail *Node
 }
@@ -26,8 +28,46 @@ func (list *SinglyLinkedList) Find(name string) *Node {
 	for n := list.First(); n != nil; n = n.Next() {
 		if n.Person.Name == name {
 			result = n
+			fmt.Println("Found : %v\n", name)
 			return result
 		}
 	}
 	return nil
+}
+
+// Delete node
+func (list *SinglyLinkedList) Delete(name string) {
+	node2del := list.Find(name)
+	if node2del == nil { // There is no such name
+		return
+	} else if node2del == list.head && node2del == list.tail { // when there is single one node
+		list.head = nil
+		list.tail = nil
+	} else if node2del == list.head { // deleting first node
+		list.head = node2del.Next()
+	} else { // deleting next one(even if the last node)
+		previous_node := list.First()
+		for previous_node != node2del {
+			previous_node = previous_node.Next()
+		}
+		list.tail = previous_node
+		list.tail.next = nil
+	}
+
+}
+
+// Pop last item from list
+func (list *SinglyLinkedList) Pop() (p Person, err error) {
+	if list.tail == nil {
+		err = errEmpty
+	} else {
+		previous_node := list.First()
+		for previous_node != list.tail {
+			previous_node = previous_node.Next()
+		}
+		list.tail = previous_node
+		list.tail.next = nil
+		p = list.tail.Person
+	}
+	return p, err
 }
